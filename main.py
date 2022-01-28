48c26de8c60950021542fedaddcda0f3c92be670
Tony Kirkland <tony@tonykirkland.net>
2022-01-28 01:45:49 -0500

    added simple args

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+#!/usr/bin/env python3
+
 import sys
 import base64
 import argparse
 
-if __name__ == '__main__':
-    print("PSSH Gen v.1 by m0style")
-    sys.exit(0)
+if __name__ == "__main__":
+    print("PSSH Gen v.1 by m0style\n")
+
+# TODO argparse --url, --file, --verbose
+
+cli = argparse.ArgumentParser()
+cli.add_argument("--verbose", "--v", help="enable verbose outpot", action="store_true")
+cli.add_argument("--url", help="URL to attempt MPD download from")
+cli.add_argument("--file", help="path to existing MPD")
+# cli.add_argument("--debug", help="URL to attempt MPD download from")
+args = cli.parse_args()
+if not (args.url or args.file):
+    print("Unmet requirement!  You must specify --url or --file")
+    sys.exit(1)
+elif args.file and args.url:
+    print("Too many options!  You must specify only one of --url or --file")
